8bf5487601becf46bd9f3874faeceb987b5164b2
saibotma <tobias.marschall@online.de>
2019-03-21 23:00:07 +0100

    feat/dimensions: Add doc strings & adjust example

--- lib/tio_core.dart
+++ lib/tio_core.dart
@@ -1,6 +1,6 @@
 /// Support for doing something awesome.
 ///
-/// More dartdocs go here.
+/// More dartdocs go here. TODO
 library tio_core;
 
 export 'package:tio_core/src/dimensions/dimensions.dart';
